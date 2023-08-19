ac9ce4663ef317b37249d318a2c2b460ee04c94b
Araceli Garcia <20203tn087@utez.edu.mx>
2023-08-18 19:12:06 -0600

    docs/Model Propietarios

---
+++ Models/Propietarios.cs
@@ -0,0 +1,13 @@
+namespace MyExamen.Models
+{
+    public class Propietarios
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Direccion { get; set; }
+         public string? CorreoElectronico { get; set; }
+
+         public string? Telefono { get; set; }
+    }
+}
